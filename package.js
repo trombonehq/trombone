03f2cbf444543d5b0b4ebc09ed7fafee89a7f9b9
Paul Cleary <pauljohncleary@gmail.com>
2016-04-01 22:04:45 +0100

    add var to try and fix subscription bug

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'trombonehq:trombone',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'Trombone - Hassle free user account management for Meteor',
   // URL to the Git repository containing the source code for this package.
