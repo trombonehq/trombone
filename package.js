507d600772e5fa612f9d384ab9d9c1ab94351d29
Paul Cleary <pauljohncleary@gmail.com>
2016-04-14 21:34:08 +0100

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'trombonehq:trombone',
-  version: '0.0.8',
+  version: '0.0.9',
   // Brief, one-line summary of the package.
   summary: 'Trombone - A hassle free user account management admin panel for Meteor',
   // URL to the Git repository containing the source code for this package.
