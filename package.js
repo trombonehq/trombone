d1431699e978f95b5156e1c9aa0972fa09191502
Paul Cleary <pauljohncleary@gmail.com>
2016-04-14 21:32:54 +0100

    switched to methods for getting users for performance reasons

--- package.js
+++ package.js
@@ -2,18 +2,18 @@ Package.describe({
   name: 'trombonehq:trombone',
   version: '0.0.8',
   // Brief, one-line summary of the package.
-  summary: 'Trombone - Hassle free user account management for Meteor',
+  summary: 'Trombone - A hassle free user account management admin panel for Meteor',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/trombonehq/trombone.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
-  //prodOnly: true
+  // prodOnly: true
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.2.1');
-  //api.use('ecmascript');
+  // api.use('ecmascript');
   api.use('ddp');
   api.use('alanning:roles@1.2.14');
   api.use('accounts-password');
@@ -37,8 +37,8 @@ Package.onUse(function(api) {
 
 });
 
-Package.onTest(function(api) {
-  //api.use('ecmascript');
+Package.onTest(function (api) {
+  // api.use('ecmascript');
   api.use('tinytest');
   api.use('trombonehq:trombone');
   api.addFiles('trombone-tests.js');
--- trombone.js
+++ trombone.js
@@ -1,33 +1,33 @@
 Trombone = {
-  configure: function(appId, appSecret, password) {
+  configure: function (appId, appSecret, password) {
 
     var apiURl = Meteor.settings.tromboneAPIUrl ? Meteor.settings.tromboneAPIUrl : 'https://trombone.io';
 
     var result = {};
 
-    if(typeof appId === 'undefined' || appId === null){
+    if (typeof appId === 'undefined' || appId === null) {
       result.connected = false;
       result.error = 'Invalid App Id';
       console.log('Trombone: Missing App Id, Please provide your app id as an argument to the configure function');
       return result;
     }
 
-    if(typeof appSecret === 'undefined' || appSecret === null || appSecret.length !== 43) {
+    if (typeof appSecret === 'undefined' || appSecret === null || appSecret.length !== 43) {
       result.connected = false;
       result.error = 'Invalid App Secret';
       console.log('Trombone: Invalid App Secret: ' + appSecret + ' please check your app secret matches the secret provided by https://trombone.io');
       return result;
     }
 
-    if(typeof password === 'undefined' || password === null){
+    if (typeof password === 'undefined' || password === null) {
       result.connected = false;
       result.error = 'Invalid Password';
       console.log('Trombone: Missing Password, please provide a password as an argument to the configure function. Use this password to access your app on https://trombone.io');
       return result;
     }
 
-    setupAccount(appSecret, password, function(error, result) {
-      if(error) {
+    setupAccount(appSecret, password, function (error, result) {
+      if (error) {
         console.log('Trombone Error: ' + error.message);
       } else {
         console.log('Trombone: Account configuration successful');
@@ -38,16 +38,16 @@ Trombone = {
 
     _tromboneConnection = DDP.connect(apiURl);
 
-    //wait to connect to the API and then authenticate
+    // wait to connect to the API and then authenticate
     function waitForConnection() {
-      if(!_tromboneConnection.status().connected) {
-        Meteor.setTimeout(waitForConnection, 100)
+      if (!_tromboneConnection.status().connected) {
+        Meteor.setTimeout(waitForConnection, 100);
       } else {
         result.connected = _tromboneConnection.status().connected;
-        var absoluteUrl = Meteor.absoluteUrl({secure: true});
+        var absoluteUrl = Meteor.absoluteUrl({ secure: true });
 
-        _tromboneConnection.call('authenticateCredentials', appId, appSecret, absoluteUrl, function(error, result) {
-          if(error) {
+        _tromboneConnection.call('authenticateCredentials', appId, appSecret, absoluteUrl, function (error, result) {
+          if (error) {
             console.log('Trombone Error: ' + error.message);
           } else {
             setupAnalytics(appId, appSecret);
@@ -61,4 +61,4 @@ Trombone = {
 
   }
 
-}
+};
